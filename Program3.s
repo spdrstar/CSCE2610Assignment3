b1166a7f6f6838d41988ef536672b6468aa4562f
Zach Eisenhauer <spdrstar@gmail.com>
2017-05-02 01:56:20 -0500

    Update Program3.s

--- Program3.s
+++ Program3.s
@@ -2,11 +2,11 @@
 
 .global_main:
 main:
-	LDR R2, =Y	         @Loads the address of Y into R
+	LDR R2, =Y	     @Loads the address of Y into R
   	LDR R2, [R2]         @Loads the value of Y into R
 	MOV R3, #0           @Initialize Q to 0
-	LDR R4, =X     		 @Loads the address of X into X reg.
-	LDR R4, [R4]   		 @Loads the value of X into X reg.
+	LDR R4, =X           @Loads the address of X into X reg.
+	LDR R4, [R4]         @Loads the value of X into X reg.
 	MOV R4, R4, LSL #16
 	MOV R5, #1           @Movesvalue of 1 into R5
 	MOV R6, #0           @Moves value of 0 into R6
@@ -15,11 +15,11 @@ main:
 	B loop               @Go to loop
 	
 loop:
-	ADD R7, R7, #01		 @increments counter value
-	CMP R7, #18			 @compare counter with its limit value
-	BEQ print_INIT 		 @if out counter reaches its end, go to printing statements
+	ADD R7, R7, #01	     @increments counter value
+	CMP R7, #18	     @compare counter with its limit value
+	BEQ print_INIT 	     @if out counter reaches its end, go to printing statements
 	SUB R8, R8, R4       @Set R = R - X
-	CMP R8, #0		     @Compares (R - X) to 0
+	CMP R8, #0           @Compares (R - X) to 0
 	BGE shift     	     @If (R - X) >= 0, branch.
 	ADD R8, R8, R4       @If (R - X) < 0, R = R + X
 	MOV R3, R3, LSL #01  @Shift quotient the the left 1
@@ -28,7 +28,7 @@ loop:
 
 shift:
 	MOV R3, R3, LSL #01  @Shifts quotient to the left 1
-	ADD R3, R3, #01		 @Set LSB to 1
+	ADD R3, R3, #01	     @Set LSB to 1
 	MOV R4, R4, LSR #01  @shift X to the right 1
 	B loop
 	
@@ -37,7 +37,7 @@ print_INIT:
 	LDR R1, =D
 	SWI 0x69
 	
-print_Q:		         @Print quotient message
+print_Q:		     @Print quotient message
 	MOV R0, #1           @Print quotient format "(Q = "
 	LDR R1, =Q
 	SWI 0x69
@@ -46,7 +46,7 @@ print_Q:		         @Print quotient message
 	MOV R1, R3
 	SWI 0x6b
 	
-print_R:    	         @Print remainder message
+print_R:    	             @Print remainder message
 	MOV R0, #1           @Remainder format ",R = "
 	LDR R1, =R
 	SWI 0x69
@@ -62,10 +62,10 @@ print_R:    	         @Print remainder message
 	SWI 0x11
 	
 .data
-Q: .asciz "(Q = " 		 @For printing the quotient message
-R: .asciz ", R = " 		 @For printing the remainder message
-P: .asciz ")"            @For the ending parenthesis 
+Q: .asciz "(Q = " 	     @For printing the quotient message
+R: .asciz ", R = " 	     @For printing the remainder message
+P: .asciz ")"                @For the ending parenthesis 
 Y: .word 12 		     @Initialize Y
-D: .asciz "12 DIV 6 = "  @For print the initial divide message
+D: .asciz "12 DIV 6 = "      @For print the initial divide message
 X: .word 6  		     @Initialize X
 .end
